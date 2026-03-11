d2caefd20d2f81ebf457b2e952e4a7c4f7693087
sathishkumar-15 <sathishkumar011315@gmail.com>
2026-03-11 17:54:52 +0530

    Initial commit

--- Todo Backend/src/main/java/Todo Application/repository/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.codeio.HelloWorld.repository;
-
-import dev.codeio.HelloWorld.models.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//CRUD - Create, Read, Update, Delete
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-
-}
